29cce163a4b1982085a24306f5d7017a406afe92
Ted Cook <teodoro.cook@gmail.com>
2025-12-29 17:33:15 -0600

    Refactor ntp script install tasks

--- src/pokerops/monitoring/ntp.py
+++ src/pokerops/monitoring/ntp.py
@@ -10,15 +10,17 @@ app = typer.Typer(help="NTP monitoring commands")
 
 @app.command("drift")
 def ntp_drift_cmd(
-    location: str = typer.Option(..., help="Location identifier"),  # pyright: ignore[reportCallInDefaultInitializer]
-    environment: str = typer.Option(..., help="Environment name"),  # pyright: ignore[reportCallInDefaultInitializer]
-    function: str = typer.Option(..., help="Function identifier"),  # pyright: ignore[reportCallInDefaultInitializer]
-    log_id: str = typer.Option("ntp-drift", help="Log identifier"),  # pyright: ignore[reportCallInDefaultInitializer]
+    peer: str = typer.Option("time.cloudflare.com", help="NTP peer address"),
+    log_id: str = typer.Option("ntp-drift", help="Log identifier"),
+    location: str = typer.Option("", help="Location identifier"),
+    environment: str = typer.Option("", help="Environment name"),
+    function: str = typer.Option("", help="Function identifier"),
 ) -> None:
-    return ntp_drift(location, environment, function, log_id)
+    return ntp_drift(peer, location, environment, function, log_id)
 
 
 def ntp_drift(
+    peer: str,
     location: str,
     environment: str,
     function: str,
@@ -26,18 +28,21 @@ def ntp_drift(
 ):
     client = ntplib.NTPClient()
     reply = client.request("time.cloudflare.com")  # pyright: ignore[reportUnknownMemberType]
-    data = {
+    drift = {
         "@timestamp": datetime.datetime.fromtimestamp(reply.tx_time, datetime.timezone.utc).isoformat(),
-        "ntp_peer_address": "time.cloudflare.com",
+        "ntp_peer_address": peer,
         "ntp_peer_offset": abs(reply.offset),
         "host": {
             "name": platform.node(),
         },
         "fields": {
-            "location": location,
-            "environment": environment,
-            "function": function,
             "log": {"description": log_id},
         },
     }
+    log = {"log": {"description": log_id}}
+    location = {} if location == "" else {"location": location}
+    environment = {} if environment == "" else {"environment": environment}
+    function = {} if function == "" else {"function": function}
+    fields = {"fields": {**log, **location, **environment, **function}}
+    data = {**drift, **fields}
     print(json.dumps(data))
